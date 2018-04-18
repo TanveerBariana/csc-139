d525775cfd3e88d16ca2bdd280bf29f8a8323b82
Tanveer Bariana <tanveerbariana@gmail.com>
2018-04-17 22:21:10 -0700

    finish change of 3

--- proj3/base.c
+++ proj3/base.c
@@ -1,53 +1,57 @@
 #include<stdio.h>
+int count,j,n,time,remain,flag=0,time_quantum= 2;
+int wait_time=0,turnaround_time=0,at[10],bt[10],rt[10];
 
-int main()
-{
+int GetMyId(int i){
+  //arrival time is synonomouse wioth id for this implementation as the order they come in is one per second
+  return at[i];
+}
+int GetStatus(int j){
+    printf("\n\nProcess\t|Turnaround Time|Waiting Time\n\n");
+        if(rt[j]<=time_quantum && rt[j]>0){
+          time+=rt[j];
+          rt[j]=0;
+          flag=1;
+        }
+        else if(rt[j]>0){
+          rt[j]-=time_quantum;
+          time+=time_quantum;
+        }
+        if(rt[j]==0 && flag==1){
+          printf("P[%d]\t|\t%d\t|\t%d\n",j+1,time-GetMyId(j),time-GetMyId(j)-bt[j]);
+          wait_time+=time-GetMyId(j)-bt[j];
+          turnaround_time+=time-GetMyId(j);
+          flag=0;
+        }
+    return GetMyId(j);
+}
+int CreateThreads(){
+    printf("4 Total:\t ");
+    n=4;
+    remain=n;
+    //arrival time and burst time
+    for(count=0;count<n;count++){
+        &at[0]=0;
+        &bt[0]=9;
+        &at[1]=1;
+        &bt[1]=5;
+        &at[2]=2;
+        &bt[2]=3;
+        &at[3]=3;
+        &bt[3]=4;
+        rt[count]=bt[count];
+    }
+    return 0;
+}
 
-  int count,j,n,time,remain,flag=0,time_quantum;
-  int wait_time=0,turnaround_time=0,at[10],bt[10],rt[10];
-  printf("Enter Total Process:\t ");
-  scanf("%d",&n);
-  remain=n;
-  for(count=0;count<n;count++)
-  {
-    printf("Enter Arrival Time and Burst Time for Process Process Number %d :",count+1);
-    scanf("%d",&at[count]);
-    scanf("%d",&bt[count]);
-    rt[count]=bt[count];
+void GO() {
+  CreateThreads();
+  for(count=0;count<n;count++){
+    GetStatus(count)
   }
-  printf("Enter Time Quantum:\t");
-  scanf("%d",&time_quantum);
-  printf("\n\nProcess\t|Turnaround Time|Waiting Time\n\n");
-  for(time=0,count=0;remain!=0;)
-  {
-    if(rt[count]<=time_quantum && rt[count]>0)
-    {
-      time+=rt[count];
-      rt[count]=0;
-      flag=1;
-    }
-    else if(rt[count]>0)
-    {
-      rt[count]-=time_quantum;
-      time+=time_quantum;
-    }
-    if(rt[count]==0 && flag==1)
-    {
-      remain--;
-      printf("P[%d]\t|\t%d\t|\t%d\n",count+1,time-at[count],time-at[count]-bt[count]);
-      wait_time+=time-at[count]-bt[count];
-      turnaround_time+=time-at[count];
-      flag=0;
-    }
-    if(count==n-1)
-      count=0;
-    else if(at[count+1]<=time)
-      count++;
-    else
-      count=0;
-  }
-  printf("\nAverage Waiting Time= %f\n",wait_time*1.0/n);
-  printf("Avg Turnaround Time = %f",turnaround_time*1.0/n);
-
+}
+int main(){
+  CreateThreads();
+  GO();
   return 0;
 }
